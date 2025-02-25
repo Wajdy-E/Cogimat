4abf4f438d082c60e92895e00c0cb79d55580547
Wajdy-E <welso040@uottawa.ca>
2025-02-24 21:41:51 -0500

    auth

---
+++ backend/generateAppleClientSecret.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const teamId = "V3VL862DLJ"; // Your Team ID
+const keyId = "7U6Z3RR66G"; // Your Key ID
+const clientId = "com.cogipro.service"; // Your Service ID
+const privateKey = process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, "\n"); // Load private key from environment variable
+
+// Function to generate Apple Client Secret
+const generateAppleClientSecret = () => {
+	return jwt.sign(
+		{
+			iss: teamId,
+			iat: Math.floor(Date.now() / 1000),
+			exp: Math.floor(Date.now() / 1000) + 15777000, // ~6 months
+			aud: "https://appleid.apple.com",
+			sub: clientId,
+		},
+		privateKey,
+		{
+			algorithm: "ES256",
+			keyid: keyId,
+		}
+	);
+};
+
+const appleClientSecret = generateAppleClientSecret();
+console.log("Apple Client Secret:", appleClientSecret);
